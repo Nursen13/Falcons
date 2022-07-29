83145012dd43da531a35b80905fa55588c17d931
Nursen13 <106557051+Nursen13@users.noreply.github.com>
2022-07-28 19:33:07 -0600

    p2c 4thTypeCommunication

--- force-app/main/default/lwc/progressBar/progressBar.js
+++ force-app/main/default/lwc/progressBar/progressBar.js
@@ -2,4 +2,9 @@ import { api, LightningElement } from 'lwc';
 
 export default class ProgressBar extends LightningElement {
     @api progressValue;
+
+    //methods can be public as well
+    @api resetProgress (){
+        this.progressValue = 50;
+    }
 }
--- force-app/main/default/lwc/progressBarParent/progressBarParent.js
+++ force-app/main/default/lwc/progressBarParent/progressBarParent.js
@@ -1,3 +1,5 @@
+//4th type of communication from parent to child
+
 import { LightningElement } from 'lwc';
 
 export default class ProgressBarParent extends LightningElement {
@@ -5,5 +7,14 @@ export default class ProgressBarParent extends LightningElement {
 
     changeHandler(event){
         this.value = event.target.value;
+    }
+    //like in apex if calling a nonstatic method, create an instance
+    resetprogressBar(){
+
+        //to achieve sync to have parent the same valuet
+        this.value= 50;
+        //if you go to template go to progress bar and fetch that method
+        this.template.querySelector('c-progress-bar').resetProgress();
+
     }
 }
